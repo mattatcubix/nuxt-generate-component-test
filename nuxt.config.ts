c386248d9297eb9c811545a204ee30566d8646e2
mattatcubix <matthew@cubix.co.nz>
2022-09-01 20:55:24 +1200

    Fix path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   components: {
-    dirs: ['~/custom', 'components', 'storyblok'],
+    dirs: ['~/custom', '~/components'],
     global: true,
   },
 });
